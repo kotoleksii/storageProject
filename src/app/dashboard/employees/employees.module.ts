34bc15483194151f3a3cd3f5a969aca7898c27ad
Oleksii Kot <akkot08@gmail.com>
2021-10-11 20:40:20 +0300

    fix: paginate page employee

--- src/app/dashboard/employees/employees.module.ts
+++ src/app/dashboard/employees/employees.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EmployeesComponent} from "./employees.component";
 import {MatTableModule} from "@angular/material/table";
-import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatPaginatorIntl, MatPaginatorModule} from "@angular/material/paginator";
 import {EmployeeRoutingModule} from "./employee-routing.module";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSortModule} from "@angular/material/sort";
@@ -10,6 +10,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {AddModalEmployeeModule} from "../../shared/components/add-modal-employee/add-modal-employee.module";
+import {MyCustomPaginatorIntl} from "../storage/storage.component";
 
 
 
@@ -26,6 +27,9 @@ import {AddModalEmployeeModule} from "../../shared/components/add-modal-employee
     MatButtonModule,
     MatIconModule,
     AddModalEmployeeModule,
+  ],
+  providers: [
+    {provide: MatPaginatorIntl, useClass: MyCustomPaginatorIntl}
   ]
 })
 export class EmployeesModule {
